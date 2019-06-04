f7eed6360c23a6aee45c7d300a5faa74e1427ed5
yeonyang9421 <chris09+>
2019-06-04 21:50:55 +0900

    06월 4일까지

--- app/src/main/java/kr/co/woobi/imyeon/instaclone/AccountFragment.kt
+++ app/src/main/java/kr/co/woobi/imyeon/instaclone/AccountFragment.kt
@@ -1,14 +1,18 @@
 package kr.co.woobi.imyeon.instaclone
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment() {
+    private lateinit var googleSignInClient: GoogleSignInClient
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,19 +20,34 @@ class AccountFragment : Fragment() {
     ): View? {
 
         setHasOptionsMenu(true)
+
+        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.account_menu,menu)
+        inflater.inflate(R.menu.account_menu, menu)
 
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_logout){
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient.signOut()
 
+            //로그인 화면으로 이동
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            requireActivity().finish()
 
-        return super.onOptionsItemSelected(item)
+        }
+            return super.onOptionsItemSelected(item)
     }
 
 
